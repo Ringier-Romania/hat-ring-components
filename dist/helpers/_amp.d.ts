eff4503aa7f42e76f4f3cf4d24ec0c24b5000da5
Arkadiusz <arkadiusz.fus@quantoo.pl>
2023-04-11 12:02:01 +0200

    fix

--- dist/helpers/_amp.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { AbstractWidgetConfig } from "../types/types";
-export declare function shouldHideWidget(widgetConfig: any, context: any): boolean;
-export declare function renderEmptyWidget(widgetConfig: any, text?: string): JSX.Element;
-export declare function renderEmptyComponent(componentClassName: any, text?: string): JSX.Element;
-export declare function getWidgetCssClasses(widgetConfig: AbstractWidgetConfig, additionalCssClasses?: Array<string>): string;
