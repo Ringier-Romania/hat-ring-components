db75b83aa898919579cca8b6731357f29a4de547
galusmarcin87 <galusmarcin87@gmail.com>
2023-08-11 11:34:40 +0200

    debug 3

--- src/components/widgets/common/BasicWidget/generalParts/SectionElements.tsx
+++ src/components/widgets/common/BasicWidget/generalParts/SectionElements.tsx
@@ -17,6 +17,7 @@ export default function SectionElements(
         }) {
 
     const elementsToRender = _.get(response, 'data.section.items.edges', _.get(response, 'data.sectionGroup.sections[0].items.edges', []));
+    return <>tutaj 3 {JSON.stringify(elementsToRender)}</>
     if (_.get(elementsToRender, 'length', 0) === 0) {
         return WidgetHelper_renderEmptyComponent('SectionElements');
     }
@@ -35,7 +36,30 @@ export default function SectionElements(
     }
     return (
         <div className={['SectionElements', bigElementsClass, columnsClass].join(' ')}>
-            tutaj 2
+            {elementsToRender.map((edge, itemIndex) => {
+                const itemParts = widgetConfig.showOptions && widgetConfig.showOptions.map((showOption, index) => {
+                    const Component = allItemParts[_.upperFirst(showOption)];
+                    if (!Component) {
+                        console.error(`No item part support ${showOption}`);
+                        return WidgetHelper_renderEmptyComponent(_.upperFirst(showOption), "item part not supported, yet");
+                    }
+
+                    return <Component key={index} itemIndex={itemIndex} context={context} widgetConfig={widgetConfig}
+                                      data={edge.node}/>;
+                });
+                const isBig = itemIndex < bigElementsCount;
+                const colClass = isBig ? `col12` : `col${colNumber}`;
+                const bigElementClass = isBig ? `bigElement` : '';
+
+                return <div className={['Item', colClass, bigElementClass].join(' ')}>
+                    <div className={'linkOverlay'}>
+                        <RingLink href={edge.node?.url || '#'} title={edge.node?.title || ''}>
+                            {widgetConfig.linkLabel}
+                        </RingLink>
+                    </div>
+                    {itemParts}
+                </div>;
+            })}
         </div>
     )
 }
