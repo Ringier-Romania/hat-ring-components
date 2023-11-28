3b6a378f0e0311754e05b2f80c6c40af1e5ca9f7
Arkadiusz <arkadiusz.fus@quantoo.pl>
2023-11-28 14:09:05 +0100

    Update CacheHelper.ts

--- src/helpers/CacheHelper.ts
+++ src/helpers/CacheHelper.ts
@@ -1,16 +1,21 @@
 import {UtilsHelper_convertToInt} from "./UtilsHelper";
 
 export function CacheHelper_set(key: any, value: any) {
+    if (process.env.CACHE_TTL === '0') {
+        return;
+    }
     if(!global.HATcache){
         global.HATcache = [];
     }
     key = JSON.stringify(key);
-    // @ts-ignore
     global.HATcache[key] = value;
     global.HATcache[`timeStamp_${key}`] = new Date().getTime();
 }
 
 export function CacheHelper_get(key: any){
+    if (process.env.CACHE_TTL === '0') {
+        return null;
+    }
     handleCleanCache();
     key = JSON.stringify(key);
     if(global.HATcache && global.HATcache[key]){
@@ -21,13 +26,16 @@ export function CacheHelper_get(key: any){
 }
 
 export function CacheHelper_runCallbackIfTimeStampHasExpired(key: any, callback: Function){
+    if (process.env.CACHE_TTL === '0') {
+        return;
+    }
     if(!global.HATcache){
         global.HATcache = [];
     }
     key = JSON.stringify(key);
     const timeStamp = global.HATcache[`timeStamp_${key}`];
     const currentTime = new Date().getTime();
-    const TTL = process.env.KEY_CACHE_TTL ? UtilsHelper_convertToInt(process.env.KEY_CACHE_TTL) : 60;
+    const TTL = process.env.CACHE_TTL ? UtilsHelper_convertToInt(process.env.CACHE_TTL) : 60;
 
     if (timeStamp && (currentTime - timeStamp >= (TTL * 1000))) {
         callback();
@@ -40,7 +48,7 @@ function handleCleanCache(){
         global.lastHATCacheClean = currentTime;
     }
 
-    const TTL = process.env.GLOBAL_CACHE_TTL ? UtilsHelper_convertToInt(process.env.GLOBAL_CACHE_TTL) : 60;
+    const TTL = process.env.CACHE_CLEAN_INTERVAL ? UtilsHelper_convertToInt(process.env.CACHE_CLEAN_INTERVAL) : 60;
     if(currentTime - global.lastHATCacheClean > (TTL * 1000)){
         global.HATcache = [];
         global.lastHATCacheClean = currentTime;
