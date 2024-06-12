619d5d0ebf71cb62fd3e3b0682f2af3c6751ae85
galusmarcin87 <galusmarcin87@gmail.com>
2024-06-12 15:38:32 +0200

    image fix

--- src/components/common/RingImage.tsx
+++ src/components/common/RingImage.tsx
@@ -42,7 +42,7 @@ export function RingImage(props) {
 
     return <>
         <picture>
-            <img {...props}/>
+            <img {...props} className={['RingImage', styles.RingImage, props.className].join(' ')}/>
         </picture>
         {props.priority && <RingImagePreload src={src}/>}
     </>
