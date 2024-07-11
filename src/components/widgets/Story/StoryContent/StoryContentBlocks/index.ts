48222acc869f50114cac95feabb48831ced62b75
Dawid Pers <dpers@grupa.onet>
2024-07-11 10:30:30 +0200

    refactor tsx to astro

--- src/components/widgets/Story/StoryContent/StoryContentBlocks/index.ts
+++ src/components/widgets/Story/StoryContent/StoryContentBlocks/index.ts
@@ -1,12 +1,12 @@
 export { default as ImageBlock } from './ImageBlock.astro';
-export { default as ParagraphBlock } from './ParagraphBlock';
-export { default as HeadingBlock } from './HeadingBlock';
-export { default as UnorderedListBlock } from './UnorderedListBlock';
-export { default as OrderedListBlock } from './OrderedListBlock';
+export { default as ParagraphBlock } from './ParagraphBlock.astro';
+export { default as HeadingBlock } from './HeadingBlock.astro';
+export { default as UnorderedListBlock } from './UnorderedListBlock.astro';
+export { default as OrderedListBlock } from './OrderedListBlock.astro';
 //@TODO optimize to have only one request for pulseembed
 export { default as EmbeddedApplicationBlock } from './EmbeddedApplicationBlock';
-export { default as TableBlock } from './TableBlock';
+export { default as TableBlock } from './TableBlock.astro';
 export { default as GroupBlock } from './GroupBlock.astro';
-export { default as PreformattedBlock } from './PreformattedBlock';
+export { default as PreformattedBlock } from './PreformattedBlock.astro';
 export { default as NotHandledBlock } from './NotHandledBlock';
 export { default as SlotBlock } from './SlotBlock.astro';
