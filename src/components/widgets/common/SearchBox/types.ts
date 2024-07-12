b6b4a29396a7bbf2b9521479d4f73bbe32c40b21
Dawid Pers <dpers@grupa.onet>
2024-07-12 13:05:56 +0200

    add SearchBox widget

---
+++ src/components/widgets/common/SearchBox/types.ts
@@ -0,0 +1,7 @@
+import { AbstractWidgetConfig } from "../../../../types/types";
+
+export interface SearchBoxWidgetConfig extends AbstractWidgetConfig {
+    placeholder: string,
+    searchURLPhrase: string,
+    searchParamPhrase: string,
+}
