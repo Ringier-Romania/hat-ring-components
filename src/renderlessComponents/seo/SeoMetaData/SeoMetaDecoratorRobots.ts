889bd351779650629bfbbce419c61e9760fd7224
Dawid Pers <dpers@grupa.onet>
2024-08-12 09:40:11 +0200

    refactor

--- src/renderlessComponents/seo/SeoMetaData/SeoMetaDecoratorRobots.ts
+++ src/renderlessComponents/seo/SeoMetaData/SeoMetaDecoratorRobots.ts
@@ -1,52 +1,50 @@
-import React from "react";
-import { AppContext } from "../../../types/types";
-import { SeoHelper_checkStoryHiddenFlag } from "../../../helpers/StoryHelper";
+import React from "react"
+import {AppContext} from "../../../types/types"
+import {SeoHelper_checkStoryHiddenFlag} from "../../../helpers/StoryHelper"
 
 const removeRobotsTag = (metaTags) => {
-    return metaTags.filter((tag: { name: string }) => tag.name !== "robots");
-};
+    return metaTags.filter((tag: {name: string}) => tag.name !== "robots")
+}
 
 export async function SeoMetaDecoratorRobots(context: AppContext, generatedMetaTags) {
     if (!generatedMetaTags) {
-        return generatedMetaTags;
+        return generatedMetaTags
     }
 
     const customMetaRobotsTags = generatedMetaTags.extend?.meta
-        ?.find((tag: { name: string }) => tag.name === "robots")
-        ?.content?.split(",");
+        ?.find((tag: {name: string}) => tag.name === "robots")
+        ?.content?.split(",")
 
-    if (!customMetaRobotsTags) {
-        return generatedMetaTags;
-    }
-
-    const isHiddenFlag = await SeoHelper_checkStoryHiddenFlag(context);
-    if (isHiddenFlag) {
-        generatedMetaTags.extend.meta = removeRobotsTag(generatedMetaTags.extend.meta);
-        return generatedMetaTags;
+    if (customMetaRobotsTags) {
+        const isHiddenFlag = await SeoHelper_checkStoryHiddenFlag(context)
+        if (isHiddenFlag) {
+            generatedMetaTags.extend.meta = removeRobotsTag(generatedMetaTags.extend.meta)
+            return generatedMetaTags
+        }
     }
 
     const customSettings = {
         nofollow: customMetaRobotsTags.includes("nofollow")
             ? true
             : customMetaRobotsTags.includes("follow")
-            ? false
-            : null,
+              ? false
+              : null,
         noindex: customMetaRobotsTags.includes("noindex")
             ? true
             : customMetaRobotsTags.includes("index")
-            ? false
-            : null,
-    };
+              ? false
+              : null,
+    }
 
-    generatedMetaTags.nofollow = customSettings.nofollow ?? generatedMetaTags.nofollow;
-    generatedMetaTags.noindex = customSettings.noindex ?? generatedMetaTags.noindex;
+    generatedMetaTags.nofollow = customSettings.nofollow ?? generatedMetaTags.nofollow
+    generatedMetaTags.noindex = customSettings.noindex ?? generatedMetaTags.noindex
 
     if (
         generatedMetaTags.nofollow === customSettings.nofollow &&
         generatedMetaTags.noindex === customSettings.noindex
     ) {
-        generatedMetaTags.extend.meta = removeRobotsTag(generatedMetaTags.extend.meta);
+        generatedMetaTags.extend.meta = removeRobotsTag(generatedMetaTags.extend.meta)
     }
 
-    return generatedMetaTags;
+    return generatedMetaTags
 }
