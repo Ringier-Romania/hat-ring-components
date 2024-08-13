4ef95c7b6172fce407111ce2429f43a2e358ca9a
Dawid Pers <dpers@grupa.onet>
2024-08-13 13:20:07 +0200

    refactor

--- src/renderlessComponents/seo/SeoMetaData/SeoMetaDecoratorRobots.ts
+++ src/renderlessComponents/seo/SeoMetaData/SeoMetaDecoratorRobots.ts
@@ -2,36 +2,48 @@ import React from "react"
 import {AppContext} from "../../../types/types"
 import {SeoHelper_checkStoryHiddenFlag} from "../../../helpers/StoryHelper"
 
-const removeRobotsTag = (metaTags) => {
-    return metaTags.filter((tag: {name: string}) => tag.name !== "robots")
+const removeNoIndexNoFollow = (metaTags) => {
+    return metaTags.filter((tag) => {
+        return !(
+            tag.name === "robots" &&
+            (tag.content.includes("index") ||
+                tag.content.includes("noindex") ||
+                tag.content.includes("follow") ||
+                tag.content.includes("nofollow"))
+        )
+    })
+}
+
+const customRobotSettings = (tags) => {
+    return {
+        nofollow: tags.includes("nofollow") ? true : tags.includes("follow") ? false : null,
+        noindex: tags.includes("noindex") ? true : tags.includes("index") ? false : null,
+    }
 }
 
 export async function SeoMetaDecoratorRobots(context: AppContext, generatedMetaTags) {
     if (!generatedMetaTags) {
         return generatedMetaTags
     }
-    const customMetaRobotsTags = generatedMetaTags.extend?.meta
-        ?.find((tag: {name: string}) => tag.name === "robots")
-        ?.content?.split(",")
-    
-    if (customMetaRobotsTags && customMetaRobotsTags.length > 0) {
+
+    const customMetaRobotsTags =
+        generatedMetaTags.extend?.meta.find((tag) => tag.name === "robots")?.content?.split(",") || []
+
+    if (customMetaRobotsTags.length > 0) {
         const isHiddenFlag = await SeoHelper_checkStoryHiddenFlag(context)
         if (isHiddenFlag) {
             generatedMetaTags.nofollow = true
             generatedMetaTags.noindex = true
-            generatedMetaTags.extend.meta = removeRobotsTag(generatedMetaTags.extend.meta)
+            generatedMetaTags.extend.meta = removeNoIndexNoFollow(generatedMetaTags.extend.meta)
             return generatedMetaTags
         }
 
-        const customSettings = {
-            nofollow: customMetaRobotsTags.includes("nofollow"),
-            noindex: customMetaRobotsTags.includes("noindex"),
-        }
+        const customSettings = customRobotSettings(customMetaRobotsTags)
+        generatedMetaTags.nofollow = customSettings.nofollow ?? generatedMetaTags.nofollow
+        generatedMetaTags.noindex = customSettings.noindex ?? generatedMetaTags.noindex
 
-        generatedMetaTags.nofollow = customSettings.nofollow
-        generatedMetaTags.noindex = customSettings.noindex
-
-        generatedMetaTags.extend.meta = removeRobotsTag(generatedMetaTags.extend.meta)
+        generatedMetaTags.extend.meta = removeNoIndexNoFollow(generatedMetaTags.extend.meta)
     }
+
     return generatedMetaTags
 }
