f7abb01ead25fe4f336f64098d0a8095931b66cb
Dawid <Dawid.Pers@ringieraxelspringer.pl>
2024-08-27 09:51:53 +0200

    fix

--- src/components/widgets/Story/StoryFrame/StoryFrameWebsitesConfig.ts
+++ src/components/widgets/Story/StoryFrame/StoryFrameWebsitesConfig.ts
@@ -15,6 +15,7 @@ export let StoryFrameWebsitesConfig
             "defaultParams": {
                 ...AbstractWebsitesWidgetConfigDefaultParams,
                 "widgetType": "storyFrame",
+                "frameCodeName": ""
             },
             "paramsDescription": {
                 ...AbstractWebsitesWidgetConfigParamsDescription,
