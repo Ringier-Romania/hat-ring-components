50944737ad9890d4d33473b89f3da71fb039f01c
dpers <dawid.pers@ringieraxelspringer.pl>
2024-11-05 17:54:38 +0100

    fix

--- src/components/widgets/common/BasicWidget/itemParts/Lead.tsx
+++ src/components/widgets/common/BasicWidget/itemParts/Lead.tsx
@@ -19,7 +19,7 @@ export default function Lead(
 
     return (
         <div className={['Lead'].join(' ')}>
-                <span>{data.lead}</span>
+            <span dangerouslySetInnerHTML={{ __html: data?.lead }}></span>
         </div>
     )
 }
