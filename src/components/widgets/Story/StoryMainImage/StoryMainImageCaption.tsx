2250faae1d06ad38608f27cb4373ff4222558e93
galusmarcin87 <galusmarcin87@gmail.com>
2024-12-15 16:04:53 +0100

    image caption with HTML

--- src/components/widgets/Story/StoryMainImage/StoryMainImageCaption.tsx
+++ src/components/widgets/Story/StoryMainImage/StoryMainImageCaption.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 
 export function StoryMainImageCaption(params){
-    return <p className={'StoryMainImageCaption'}>{params.caption}</p>;
+    return <p className={'StoryMainImageCaption'} dangerouslySetInnerHTML={{__html:params.caption}}></p>;
 }
 
