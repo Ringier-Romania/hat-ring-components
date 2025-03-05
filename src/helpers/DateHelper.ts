4f4e8197243541aaa655fe094101a2fe819a26d4
Dawid Pers <dawid.pers@ringieraxelspringer.pl>
2025-03-05 12:24:47 +0100

    fix

--- src/helpers/DateHelper.ts
+++ src/helpers/DateHelper.ts
@@ -54,12 +54,10 @@ function importDayJs(locale: string) {
 
 export async function DateHelper_fromNow(context: AppContext, date: string, dateTemplate?: any): Promise<string> {
     dayjs.extend(relativeTime);
-    dayjs.extend(utc);
     dayjs.extend(timezone);
     const destinationLanguage = await ConfigHelper_getLanguage(context);
     const dateSettings = await ConfigHelper_getDateFormatConfig(context);
     const timeZone = dateSettings ? dateSettings.timeZone : "Europe/London";
-    dayjs().tz(timeZone).utc();
     if (dateTemplate) {
         const localLocale = {
             ...dayjs.Ls[destinationLanguage],
@@ -68,8 +66,8 @@ export async function DateHelper_fromNow(context: AppContext, date: string, date
             relativeTime: dateTemplate.relativeTime,
         };
         delete dayjs.Ls["fromNow-temp"];
-        return dayjs(date).locale(localLocale).fromNow();
+        return dayjs(date).tz(timeZone).locale(localLocale).fromNow();
     }
 
-    return dayjs(date).locale(destinationLanguage).fromNow();
+    return dayjs(date).tz(timeZone).locale(destinationLanguage).fromNow();
 }
