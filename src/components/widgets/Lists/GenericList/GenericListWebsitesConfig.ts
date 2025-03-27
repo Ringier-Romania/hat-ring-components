c35c1b12fc4a677cc7e97565e9ffc8c9aa5e92a8
Dawid Pers <dawid.pers@ringieraxelspringer.pl>
2025-03-27 16:15:49 +0100

    fix

--- src/components/widgets/Lists/GenericList/GenericListWebsitesConfig.ts
+++ src/components/widgets/Lists/GenericList/GenericListWebsitesConfig.ts
@@ -37,7 +37,7 @@ export let GenericListWebsitesConfig
                 "customTeasers": [],
                 "moreText": "",
                 "moreUrl": "",
-                "titleAddonsCodeNames": ""
+                "titleAddonsCodeNames": "",
             },
             "paramsDescription": {
                 ...AbstractWebsitesWidgetConfigParamsDescription,
