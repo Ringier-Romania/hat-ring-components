46116e6b50e7fcda1a43cef4ff6a22756d20695c
Arkadiusz Fus <arkadiusz.fus2@ringieraxelspringer.pl>
2025-05-13 12:16:53 +0200

    Breadcrumbs

--- src/components/widgets/common/Breadcrumbs/BreadcrumbsWebsitesConfig.ts
+++ src/components/widgets/common/Breadcrumbs/BreadcrumbsWebsitesConfig.ts
@@ -27,12 +27,12 @@ export let BreadCrumbpsWebsitesConfig
                 },
                 "firstLevelText": {
                     "name": "Custom first level text",
-                    "description": "Text displayed for the first navigation level",
+                    "description": "Text displayed for the first navigation level. (It's second level when \"Disable 1st level\" option is enabled)",
                     "type": "textfield"
                 },
                 "firstLevelUrl": {
                     "name": "Custom first level url",
-                    "description": "URL address for the first level navigation item",
+                    "description": "URL address for the first level navigation item. (It's second level when \"Disable 1st level\" option is enabled)",
                     "type": "textfield"
                 },
             }
