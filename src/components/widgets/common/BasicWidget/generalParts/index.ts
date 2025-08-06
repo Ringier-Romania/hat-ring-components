447db46475260782ba2f0f2be4aa1879b73fbd8a
Arkadiusz Fus <arkadiusz.fus2@ringieraxelspringer.pl>
2025-08-06 08:36:27 +0200

    3.3.1

--- src/components/widgets/common/BasicWidget/generalParts/index.ts
+++ src/components/widgets/common/BasicWidget/generalParts/index.ts
@@ -2,5 +2,5 @@
 export { default as SectionElements } from './SectionElements.astro';
 export { default as ListElements } from './ListElements/ListElements.astro';
 export { default as Header } from './Header.astro';
-export { default as Description } from './Description';
+export { default as Description } from './Description.astro';
 export { default as Button } from './Button';
